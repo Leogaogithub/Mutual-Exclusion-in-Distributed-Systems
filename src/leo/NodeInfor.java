39251effc0bc3ea78e023f8cf2ae088884bbc33a
leo <longhaogao@gmail.com>
2016-07-09 21:42:52 -0500

    ok

--- src/leo/NodeInfor.java
+++ src/leo/NodeInfor.java
@@ -1,25 +1,25 @@
 package leo;
 public class NodeInfor {
-	int nodeId;
-	String hostName;
-	int port;
-	String ipAddress;
+	public int nodeId;
+	public String hostName;
+	public int port;
+	public String ipAddress;
 	
-	NodeInfor(){
+	public NodeInfor(){
 		nodeId = 0;
 		port = 0;
 		hostName = "";
 		ipAddress = "";
 	}
 	
-	NodeInfor(int id, String hostName, int port, String ipAddress){
+	public NodeInfor(int id, String hostName, int port, String ipAddress){
 		this.nodeId = id;
 		this.port = port;
 		this.hostName = hostName;
 		this.ipAddress = ipAddress;
 	}
 	
-	NodeInfor(int id, String hostName, int port){
+	public NodeInfor(int id, String hostName, int port){
 		this.nodeId = id;
 		this.port = port;
 		this.hostName = hostName;		
